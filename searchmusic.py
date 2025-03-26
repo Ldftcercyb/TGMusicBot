49fe3b6943a1b53e363f471e1886017a3e0a9964
Ldftcer-Elbakyan-homewoork-classrom <156641760+Ldftcercyb@users.noreply.github.com>
2025-03-26 17:14:04 +0400

    Update searchmusic.py

--- searchmusic.py
+++ searchmusic.py
@@ -6,7 +6,7 @@ import re
 from concurrent.futures import ThreadPoolExecutor
 
 TOKEN = '7939631781:AAGBR38TykN2YyIh8dh2qQSRM11FtXGqTVY'  # Փոխարինեք ձեր բոտի նշանաբառով
-OWNER_ID = 5743254515  # Փոխարինեք ձեր Telegram ID-ով
+OWNER_ID = 5743254515 
 BANNED_USERS_FILE = 'banned_users.txt'
 
 bot = telebot.TeleBot(TOKEN)
